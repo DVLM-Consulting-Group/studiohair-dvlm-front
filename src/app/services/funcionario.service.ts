0ab026e8a93405c6462b0252a93415dfff32f11b
Luan Braz <luan_silvacpv@hotmail.com>
2022-09-02 20:53:53 -0300

    valid forms

--- src/app/services/funcionario.service.ts
+++ src/app/services/funcionario.service.ts
@@ -1,8 +1,9 @@
+import { Funcionario } from './../models/funcionario';
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Funcionario } from '../models/funcionario';
 import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +12,25 @@ export class FuncionarioService {
 
   baseUrl: String = environment.baseUrl;
 
-  constructor(private http : HttpClient) { }
+  constructor(
+    private http : HttpClient,
+    private snack: MatSnackBar) { }
 
   findAll():Observable<Funcionario[]>{
     const url = this.baseUrl + "/funcionarios";
     return this.http.get<Funcionario[]>(url);
+  }
+
+  create(funcionario: Funcionario):Observable<Funcionario> {
+      const url = this.baseUrl + "/funcionarios";
+      return this.http.post<Funcionario>(url, funcionario);
+  }
+
+  message(msg: String): void {
+    this.snack.open(`${msg}`, 'OK', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 4000
+    })
   }
 }
--- src/app/views/components/funcionario/funcionario-create/funcionario-create.component.ts
+++ src/app/views/components/funcionario/funcionario-create/funcionario-create.component.ts
@@ -1,3 +1,5 @@
+import { Funcionario } from './../../../../models/funcionario';
+import { FuncionarioService } from './../../../../services/funcionario.service';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,13 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FuncionarioCreateComponent implements OnInit {
 
-  constructor(private router : Router) { }
+  funcionario: Funcionario = {
+    id: '',
+    nome: '',
+    cpf: '',
+    telefone: '',
+    email: ''
+  }
+
+  constructor(private router : Router,
+    private service : FuncionarioService) { }
 
   ngOnInit(): void {
   }
 
   cancel(): void {
     this.router.navigate(['funcionarios'])
+  }
+
+  create(): void {
+    this.service.create(this.funcionario).subscribe((resposta) => {
+      this.router.navigate(['funcionarios'])
+      this.service.message('Funcionario criado com sucesso!')
+    }, err => {
+      console.log(err)
+      if (err.error.error.match('CPF já cadastrado na base de dados!')) {
+        this.service.message(err.error.error)
+      } else if (err.error.errors[0].message === "número do registro de contribuinte individual brasileiro (CPF) inválido") {
+        this.service.message("CPF inválido!")
+        console.log(err)
+      } else {
+        console.log(err)
+      }
+    })
+    
   }
 
 }
